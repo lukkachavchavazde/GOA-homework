253ffdd819bba8a7741ec9220c265d38f33fa237
lukkachavchavazde <lukachavchavadze646@gmail.com>
2024-05-23 06:36:45 -0700

    first commit

---
+++ level 01/homework2.py
@@ -0,0 +1,34 @@
+#name="luka"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"luka" არის ცვლადის მნიშვნელობა
+
+
+surname = "chavchavadze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "luka" #ეს არის str (string) ტიპის ცვლადი
+age = 12 # ეს არის int (integer) მთელი რიცხვი
+height = 1.70 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True  #True ან False
+is_ugly = False  #snakecase (უბრალოდ წერის სტილი სტანდარტულად)
+
+isugly = False  #ჯავასკრიპტული  camelcase
+
+
+#print(name +" "+ surname)
+#სტრინგი არის ბრჭყალრბსში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programing))
+print(name + " " + surname + " " + str(age) + " " + str(height))
+
+
