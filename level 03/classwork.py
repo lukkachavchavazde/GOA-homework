8512d30cf1d46607e402848a9056dbac454a17f7
lukkachavchavazde <lukachavchavadze646@gmail.com>
2024-10-25 20:16:02 +0400

    goa

---
+++ level 03/classwork.py
@@ -0,0 +1,19 @@
+int_variable = 5
+float_variable = 3.5
+result = int_variable + float_variable
+print(result)
+
+
+
+bool = True
+bool1 = False
+result_bool = int(bool1) + int(bool)
+print(result_bool)
+
+
+
+
+variable1 = 20
+variable2 = 24
+print(2024)
+
