3496519557a9a25014932d395ab9e9b4a4f051a9
lukkachavchavazde <lukachavchavadze646@gmail.com>
2024-11-12 17:05:37 +0400

    goa

---
+++ level 51/classwork/script.js
@@ -0,0 +1,43 @@
+
+// 1  JavaScript makes websites interactiveupdates content without reloading, and builds web and mobile  
+// apps It powers features like buttons forms and games.
+
+
+// 2 event is when some action is  happening on our computer like moving the mouse ore opening a new tab and much more.
+
+
+
+
+
+//  3 In JavaScript, variables are dynamically typed, meaning their
+//  type is determined by the value they hold and can change during execution.
+
+
+// for example
+
+let x = 10;    // x is a number
+x = "Helloworld";   // now we change it to  a string
+
+
+//  Unlike C which is a statically typed language where variable types are fixed JavaScript 
+// allows variables to change their type as needed This flexibility is what makes JavaScript dynamically typed
+
+
+
+//  4 we have 3 keyworlds in javascript 
+//  first is let
+//  second number 
+// third const
+
+
+
+// 5 yes it will couse error
+//   5 You can't declare the same variable twice with let in the same place.
+
+// correct way 
+
+let num = 10;
+num = 6;
+
+
+// the camel cais is coled becouse it has capitalized letters and has bumps like 
